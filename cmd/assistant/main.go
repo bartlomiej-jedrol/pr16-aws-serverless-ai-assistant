d89f49b2a8e28ee9d7d5c16f5724362a23303f65
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2024-12-06 09:35:39 +0100

    wip

--- cmd/assistant/main.go
+++ cmd/assistant/main.go
@@ -5,13 +5,29 @@ import (
 	"encoding/json"
 	"log"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/bartlomiej-jedrol/pr16-aws-serverless-ai-assistant/telegram"
 )
 
-func handler(ctx context.Context, event json.RawMessage) error {
-	// Logging
-	log.Printf("INFO: handleRequest - event: %+v", string(event))
-	return nil
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("INFO: handler - HTTPMethod, %+v", request.HTTPMethod)
+	log.Printf("INFO: handler - Headers, %+v", request.Headers)
+	log.Printf("INFO: handler - Body, %+v", request.Body)
+
+	res := telegram.Response{
+		Text: "response",
+	}
+	body, err := json.Marshal(res)
+	if err != nil {
+		log.Printf("INFO: handler - failed to marshal body, %v", err)
+	}
+
+	r := events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       string(body),
+	}
+	return r, nil
 }
 
 func main() {
