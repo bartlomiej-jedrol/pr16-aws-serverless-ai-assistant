cef78ff9d3dbc6dfaa15a3e48a4ca90f5f386c4f
bartlomiej-jedrol <bartlomiej.jedrol@gmail.com>
2025-04-02 14:33:28 +0200

    wip

--- cmd/assistant/main.go
+++ cmd/assistant/main.go
@@ -2,36 +2,72 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
 	iAws "github.com/bartlomiej-jedrol/go-toolkit/aws"
+	iLog "github.com/bartlomiej-jedrol/go-toolkit/log"
 	"github.com/bartlomiej-jedrol/pr16-aws-serverless-ai-assistant/api"
 	"github.com/bartlomiej-jedrol/pr16-aws-serverless-ai-assistant/configuration"
 	"github.com/bartlomiej-jedrol/pr16-aws-serverless-ai-assistant/telegram"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 var (
-	awsCfg        *aws.Config
-	s3Client      *s3.Client
-	s3Bucket      = "pr16-assistant-bucket"
-	lambdaTmpPath = "/tmp"
+	awsCfg *aws.Config
 )
 
 func init() {
+	function := "init"
 	serviceName, err := iAws.GetEnvironmentVariable("SERVICE_NAME")
 	if err != nil {
 		return
 	}
 	configuration.SetServiceName(serviceName)
 
+	dbHost, err := iAws.GetEnvironmentVariable("DB_HOST")
+	if err != nil {
+		return
+	}
+	dbUser, err := iAws.GetEnvironmentVariable("DB_USER")
+	if err != nil {
+		return
+	}
+	dbPass, err := iAws.GetEnvironmentVariable("DB_PASSWORD")
+	if err != nil {
+		return
+	}
+	dbName, err := iAws.GetEnvironmentVariable("DB_NAME")
+	if err != nil {
+		return
+	}
+
 	awsCfg, err = iAws.LoadDefaultConfig()
 	if err != nil {
 		return
 	}
+
+	psqlConnStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require",
+		dbHost, 5432, dbUser, dbPass, dbName)
+
+	pool, err := pgxpool.New(context.Background(), psqlConnStr)
+	if err != nil {
+		iLog.Error("failed to create connection pool", nil, err, serviceName, function)
+		return
+	}
+	defer pool.Close()
+
+	var greeting string
+	err = pool.QueryRow(context.Background(), "SELECT 'Hello, RDS!'").Scan(&greeting)
+	if err != nil {
+		iLog.Error("failed to run query row", nil, err, serviceName, function)
+		return
+	}
+
+	iLog.Info(greeting, nil, nil, serviceName, function)
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
--- go.mod
+++ go.mod
@@ -29,7 +29,14 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.28.10 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.33.9 // indirect
 	github.com/aws/smithy-go v1.22.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.4 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/crypto v0.31.0 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
